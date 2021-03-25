af746c7e6b003c0bce9bb032a8ff1816232934b9
Emma Landry <emma.landry17@imperial.ac.uk>
2021-03-25 11:33:53 +0000

    Checking directory problem

--- code/checking_dir.R
+++ code/checking_dir.R
@@ -1,4 +1,4 @@
 filename <- "testing_file.rds"
 a<- data.frame(Vals = rep(1,10))
 
-saveRDS(res, file =  paste0(parent,"/Outputs/epidemia_fits/", filename))
+saveRDS(a, file =  paste0(parent,"/Outputs/epidemia_fits/", filename))
